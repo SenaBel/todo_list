ad8eca81dc9e389f8030d37c58b1c7ac9b61122c
Abel <abelsena1@hotmail.com>
2022-06-02 14:15:55 -0300

    finalizando o todolist

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-'use strict';
 
+// criando um banco teste
 let banco = [ 
     {tarefa: 'Treinando Js', status: ''},
     {tarefa: 'Treinando Angular Js', status: 'checked'},
     {tarefa: 'Node Js', status: ''}
 ]
 
-// criando um item no js
-const criarItem = (tarefa, status='') => {
+
+localStorage.setItem('todoList', JSON.stringify(banco)) // Enviando banco para localStorage
+const getBanco = () => JSON.parse(localStorage.getItem('todoList')) ?? [] // pegando dados do localStorage
+const setBanco = (banco) => localStorage.setItem('todoList', JSON.stringify(banco)) // função para atualizar o banco
+
+// criando os itens
+const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label')
     item.classList.add('todo__item')
     item.innerHTML = `
-        <input type="checkbox" ${status}>
+        <input type="checkbox" ${status} data-indice=${indice}>
         <div>${tarefa}</div>
-        <input type="button" value="X">
+        <input type="button" value="X" data-indice=${indice}>
     `
     document.getElementById('todoList').appendChild(item)
 }
@@ -27,34 +32,48 @@ const limparTarefas = () => {
 
 const atualizarTela = () => {
     limparTarefas()
-    banco.forEach(item => {
-        criarItem(item.tarefa, item.status)
-    } )
+    const banco = getBanco()
+    banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice))
 }
-
-// const limparTarefaNova = (event) =>{
-//    const tecla2 = event.key
-//    if(tecla2 === 'Enter'){
-//     event.target.value = ''
-//    }
-// }
 
 const inserirItem = (event) => {
     const tecla = event.key
     const texto = event.target.value
     if(tecla === 'Enter'){
+        const banco = getBanco()
         banco.push({tarefa: texto, status: ''})
-        atualizarTela()
+        setBanco(banco)
+        atualizarTela(banco)
         //limparTarefaNova()
         event.target.value = ''
     }
-
-    //console.log('Teclas digitadas: ', tecla)
 }
+
+const removeItem = (indice) => {
+    const banco = getBanco()
+    banco.splice (indice, 1)
+    setBanco(banco)
+    atualizarTela()
+}
+
+const atualizaItem = (indice) => {
+    const banco = getBanco()
+    banco[indice].status = banco[indice].status === '' ? 'checked' : ''
+    setBanco(banco)
+    atualizarTela()
+}
+
 
 const clickItem = (event) => {
     const elemento = event.target
-    console.log('valor do elemento: ', elemento)
+    if(elemento.type === "button"){
+        const indice = elemento.dataset.indice
+        removeItem(indice)
+    }else if(elemento.type === "checkbox"){
+        const indice = elemento.dataset.indice
+        atualizaItem(indice)
+    }
+    console.log('valor do elemento: ', elemento.type)
 }
 
 document.getElementById('newItem').addEventListener('keypress', inserirItem)
